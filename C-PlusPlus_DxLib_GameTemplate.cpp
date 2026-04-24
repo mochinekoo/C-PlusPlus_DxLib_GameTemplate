89c17dd2b27bf4659272f5a69d59a91cc4d259ed
mochinekoo <mochinekoo0113@gmail.com>
2026-04-24 21:14:07 +0900

    [add] CMakeでビルドできるようになりました

---
+++ C-PlusPlus_DxLib_GameTemplate.cpp
@@ -0,0 +1,13 @@
+﻿// C-PlusPlus_DxLib_GameTemplate.cpp : アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "C-PlusPlus_DxLib_GameTemplate.h"
+#include "DxLib.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
